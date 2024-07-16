a978caf98c8ccd64cbd74a670d12fa15bfafcbb0
Sandip Saha <110690069+sandipsaha2005@users.noreply.github.com>
2024-07-16 19:22:15 +0530

    sticky section

--- src/Comp/Header.jsx
+++ src/Comp/Header.jsx
@@ -6,23 +6,7 @@ const Header = () => (
     <a className="frame__back" href="https://tympanus.net/codrops/?p=75532">Article</a>
     <a className="frame__prev" href="http://tympanus.net/Development/ScrollCarousel/">Previous demo</a>
     <a className="frame__sub" href="https://tympanus.net/codrops/collective/">Subscribe to our frontend news</a>
-    <nav className="frame__demos">
-      <a href="index.html">1</a>
-      <a href="index2.html">2</a>
-      <a href="index3.html">3</a>
-      <a href="index4.html">4</a>
-      <span>5</span>
-      <a href="index6.html">6</a>
-      <a href="index7.html">7</a>
-      <a href="index8.html">8</a>
-      <a href="index9.html">9</a>
-      <a href="index10.html">10</a>
-      <a href="index11.html">11</a>
-      <a href="index12.html">12</a>
-      <a href="index13.html">13</a>
-      <a href="index14.html">14</a>
-      <a href="index15.html">15</a>
-    </nav>
+   
     <div className="frame__heading">
       <h2 className="content__title"><i>The</i> Periphery</h2>
       <p className="text-meta">An exploration of the Synthetic Era.</p>
