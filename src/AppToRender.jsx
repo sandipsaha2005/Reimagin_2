66a8bb0e411ee16808da519a53f5a6b0a549ba4b
Sandip Saha <110690069+sandipsaha2005@users.noreply.github.com>
2024-07-17 20:50:17 +0530

    new app created

--- src/AppToRender.jsx
+++ src/AppToRender.jsx
@@ -8,34 +8,21 @@ import JustCheck from './componets/JustCheck'
 import Sticky from './Comp/StickySections'
 import ScrollThreeD from './Comp/Scroll3Dgrid/Index'
 import PreLoader from './Comp/preLoader/Index'
-import AppToRender from './AppToRender'
+import VideoPlayer  from './Comp/preLoader/VideoPlayer'
+import OpenVideo from './assets/video/PreLoadder.mp4'
 // import ScrollThreeD './Comp/'
-function App() {
+function AppToRender() {
   const [count, setCount] = useState(0)
   const [isPreLoading, setIsPreLoading] = useState(true);
   const [seconds, setSeconds] = useState(0);
 
-  useEffect(() => {
-    if (seconds <10) {
-      const interval = setInterval(() => {
-        setSeconds(prevSeconds => prevSeconds + 1);
-      }, 1000);
-      if(seconds==8){
-        setIsPreLoading(false)
-      }
-      return () => clearInterval(interval);
-    }
-  }, [seconds]);
-console.log(seconds);
+
 
   return (
     <>
-      {isPreLoading ? <PreLoader /> : <AppToRender/>}
-        
-      {/* <VideoPlayer  url={OpenVideo} poster={'https://fps.cdnpk.net/images/home/subhome-ai.webp?w=649&h=649'}/> */}
-    
+      <Sticky/>
     </>
   )
 }
 
-export default App
+export default AppToRender
