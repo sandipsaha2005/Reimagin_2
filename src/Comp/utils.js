7fa302dcf397f4b12bb696e7b4fa663bda29ea5a
SangikGhosh <sangik.ghosh1@gmail.com>
2024-07-19 13:44:52 +0530

    d

--- src/Comp/utils.js
+++
@@ -1,7 +0,0 @@
-import imagesLoaded from 'imagesloaded';
-
-export const preloadImages = (selector) => {
-  return new Promise((resolve) => {
-    imagesLoaded(document.querySelectorAll(selector), { background: true }, resolve);
-  });
-};
