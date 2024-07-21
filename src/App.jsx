4785324d2893f31dc21fa333ab5941a8fe9f4603
SangikGhosh <sangik.ghosh1@gmail.com>
2024-07-21 20:18:47 +0530

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState ,useEffect} from 'react'
-import reactLogo from './assets/react.svg'
+// import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 // import './App.css'
 // import HorizontalParallax from './componets/HorizontalParallax'
